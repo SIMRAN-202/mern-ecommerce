277139325f5ebb770325bea20dc894eb0ce03ed5
SIMRAN-202 <kaursimrankaur2003@gmail.com>
2025-06-23 15:10:19 +0530

    `Added new product routes and controllers for fetching product by ID, all products, top products, new products, and adding product reviews.`

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -88,4 +88,79 @@ const fetchProducts = asyncHandler(async(req,res)=>{
     }
 })
 
-export {addProduct, updateProductDetails, deleteProduct, fetchProducts}
+const fetchProductById = asyncHandler(async(req,res)=>{
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product){
+            res.json(product)
+        }else{
+            res.status(404).json({error: 'Product not found'})
+        }
+    } catch (error) {
+        res.json(404).json({error:"Product not found"})
+    }
+})
+
+const fetchAllProducts = asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find({}).populate("category").limit(12).sort({createdAt:-1});
+        return res.json(products);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:"Server error"});
+    }
+});
+
+const addProductReview = asyncHandler(async(req,res)=>{
+    try {
+        const {rating, comment} = req.body;
+        const product = await Product.findById(req.params.id)
+        if (product){
+            const alreadyReviewed = product.reviews.find(r => r.user.toString() === req.user._id.toString())
+
+            if (alreadyReviewed){
+                res.status(400).json({error: 'Product already reviewed'})
+            }
+
+            const review = {
+                name:req.user.username,
+                rating: Number(rating),
+                comment,
+                user:req.user._id
+            }
+
+            product.reviews.push(review)
+            product.numReviews = product.reviews.length
+            product.rating = product.reviews.reduce((acc, item)=> item.rating + acc, 0) /product.reviews.length
+
+            await product.save()
+            res.json({message: 'Review added successfully'})
+        }else{
+            res.status(404).json({error: 'Product not found'})
+        }
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+})
+
+const fetchTopProducts = asyncHandler(async(req,res)=>{
+    try {
+        const products = await Product.find({}).sort({rating:-1}).limit(4);
+        res.json(products)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message)
+    }
+})
+
+const fetchNewProducts = asyncHandler(async(req,res)=>{
+    try {
+        const products = await Product.find({}).sort({ _id: -1}).limit(5);
+        res.json(products)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message)
+    }
+})
+
+export {addProduct, updateProductDetails, deleteProduct, fetchProducts, fetchProductById, fetchAllProducts, addProductReview, fetchTopProducts, fetchNewProducts}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,22 @@ import formidable from "express-formidable";
 
 const router = express.Router()
 
-import { addProduct, updateProductDetails, deleteProduct, fetchProducts } from "../controllers/productController.js";
+import { addProduct, updateProductDetails, deleteProduct, fetchProducts, fetchProductById, fetchAllProducts, addProductReview, fetchTopProducts, fetchNewProducts } from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from '../middlewares/checkId.js'
 
 router.route('/').get(fetchProducts).post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route('/allproducts').get(fetchAllProducts)
+router.route('/:id/reviews').post(authenticate, authorizeAdmin, checkId ,  addProductReview)
+router.get('/top', fetchTopProducts)
+router.get('/new', fetchNewProducts)
 
 router.route('/:id').put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
 
 router.route('/:id').delete(authenticate,authorizeAdmin, formidable(), deleteProduct);
+
+router.route('/:id').get(fetchProductById)
+
 
 export default router;
